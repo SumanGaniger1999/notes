6080621308bf1a08d8cbb9bfea2f33ac05076790
SumanGaniger1999 <89911110+SumanGaniger1999@users.noreply.github.com>
2022-01-16 22:38:14 +0530

    Add files via upload

---
+++ Laptop.java
@@ -0,0 +1,14 @@
+class Laptop{
+
+public int laptopId;
+public String name;
+public int price;
+public String memory;
+
+
+public void typing (){
+System.out.println("Typing the program");
+
+}
+}
+
